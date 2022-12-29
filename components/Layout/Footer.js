5b942676de6dd50ff5d636b3b78f04b6703c0f16
Kamil <kamil.kolatek@goodylabs.com>
2022-12-29 19:30:10 +0100

    add footer

---
+++ components/Layout/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <nav class="w-full bg-gray-100 px-2 py-4">
+    <div class="container text-right">
+      Wszelkie prawa zastrzeżone &copy; 2022 Karolina Kołatek
+    </div>
+  </nav>
+);
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import AboutSection from "../components/AboutSection/AboutSection";
 import AnnouncementsSection from "../components/Announcements/AnnouncementsSection";
 import ContactSection from "../components/ContactSection/ContactSection";
 import HeroSection from "../components/HeroSection/HeroSection";
+import Footer from "../components/Layout/Footer";
 import Navbar from "../components/Layout/Navbar";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
         <AnnouncementsSection />
         <ContactSection />
       </main>
+      <Footer />
     </>
   );
 }
